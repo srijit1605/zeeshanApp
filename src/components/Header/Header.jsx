3ad1331e0c09915e1085e6b690dedcce0d02b064
RupsaPoliRoy <88063861+RupsaPoliRoy@users.noreply.github.com>
2023-04-10 08:16:33 +0530

    git initialized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => {
   return (
     <div className='headerContainer'>
       <div className='headerSection'>
-        <div className='logo'><img src={logo} height={60} alt='Zeeshan'/><h3 className='logoText'>Zeeshan</h3></div>
+        <div className='logo'><img src={logo} height={60} alt='Zeeshan' /><h3 className='logoText'>Zeeshan</h3></div>
         <div className='headerRight'>
-        <div className='nav'>
-          {menuItems.map((menuItem) => <p className='navItem'>{menuItem}</p>)}
-        </div>
-        <div>
-          <p><a href='/'>+2345678893</a></p>
-        </div>
+          <div className='nav'>
+            {menuItems.map((menuItem) => <p className='navItem'>{menuItem}</p>)}
+          </div>
+          <div>
+            <p><a href='/'>+2345678893</a></p>
+          </div>
         </div>
       </div>
     </div>

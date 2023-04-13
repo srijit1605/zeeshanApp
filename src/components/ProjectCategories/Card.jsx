c5e09ca220ad58729ac8cfed482a919de7e0a4d7
RupsaPoliRoy <88063861+RupsaPoliRoy@users.noreply.github.com>
2023-04-13 07:36:10 +0530

    project categories card section

--- src/components/ProjectCategories/Card.jsx
+++ src/components/ProjectCategories/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./projectCategories.css";
 
-const Card = ({name}) => {
+const Card = ({ name, bg }) => {
   return (
-    <div className='cardContainer'>{name}</div>
+    <div className='cardContainer' style={{ background: `url(${bg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', borderRadius: '20px' }}>
+      <div className='cardOverlay'>
+      {name}
+      </div>
+    </div>
   )
 }
 
--- src/components/ProjectCategories/ProjectCategories.jsx
+++ src/components/ProjectCategories/ProjectCategories.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 import "./projectCategories.css";
 import Card from "./Card";
+import newLaunches from "../../assets/newLaunches.jpg"
+import upcomingProjects from "../../assets/upcomingProjects.jpg"
+import ReadyToMove from "../../assets/ReadyToMove.jpg"
+import reSaleFlats from "../../assets/reSaleFlats.jpg"
+import bunglows from "../../assets/bunglows.jpg"
+import commercial from "../../assets/commercial.jpg"
+import land from "../../assets/land.jpg"
+import investments from "../../assets/investments.jpg"
 
 const ProjectCategories = () => {
-  const categories = [
-    "New Launches",
-    "Upcoming Projects",
-    "Ready to Move",
-    "Re-sale flats",
-    "Bunglows",
-    "Commercial",
-    "Lands",
-    "Investments",
-  ];
+  const projCategories = [
+  {
+    title: "New Launches",
+    url: newLaunches
+  },
+  {
+    title: "Upcoming Projects",
+    url: upcomingProjects
+  },
+  {
+    title: "Ready to Move",
+    url: ReadyToMove
+  },
+  {
+    title: "Re-sale flats",
+    url: reSaleFlats
+  },
+  {
+    title: "Bunglows",
+    url: bunglows
+  },
+  {
+    title: "Commercial",
+    url: commercial
+  },
+  {
+    title: "Lands",
+    url: land
+  },
+  {
+    title: "Investments",
+    url: investments
+  },
+]
   return (
     <div className="ProjectBackground">
       <div className="ProjectContainer">
         <h1>Project Categories</h1>
         <h3>Choose your home based on the type you are looking for. That will make your search much easier.</h3>
-      <div className="cardSection">
-      {categories.map((category, key) => <Card key={key} name={category}/>)}
-      </div>
+        <div className="cardSection">
+          {projCategories.map((category, key) => <Card key={key} name={category.title} bg={category.url}/>)}
+        </div>
       </div>
     </div>
   );

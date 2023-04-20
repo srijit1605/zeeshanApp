62c53242a800450085d52a6459304ea11a5c8926
RupsaPoliRoy <88063861+RupsaPoliRoy@users.noreply.github.com>
2023-04-21 05:12:26 +0530

    broken router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 import Homepage from './Page/Homepage';
+// import HomepageConstruction from './Page/HomepageConstruction';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <Homepage/>
+    {/* <Router>  
+    <div>  
+      <h1>React Router Example</h1>  
+      <Routes >
+    <Route path="/" component={<Homepage/>} />  
+      <Route path="/zeeshan-construction" component={<HomepageConstruction/>} />  
+      </Routes>  
+    </div>  
+  </Router>   */}
     </div>
   );
 }
--- src/Page/Homepage.jsx
+++ src/Page/Homepage.jsx
@@ -8,13 +8,14 @@ import yt from './../assets/yt.png'
 import ln from './../assets/in.png'
 import trading from './../assets/trading.jpg'
 import construction from './../assets/construction.jpg'
-import comingSoon from './../assets/comingsoon.gif'
+import comingSoon from './../assets/comingsoon.jpg'
 
 const Homepage = () => {
     return (
         <div className='background'>
+            <div className='bgOverlay'>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <img src={logo} height={60} alt='Zeeshan' />
+                <img style={{padding: '20px'}} src={logo} height={60} alt='Zeeshan' />
                 <div className='socialButton'>
                     <img className='socialMenuItem' src={fb} width={30} height={30} />
                     <img className='socialMenuItem' src={insta} width={30} height={30} />
@@ -23,17 +24,17 @@ const Homepage = () => {
                     <img className='socialMenuItem' src={ln} width={30} height={30} />
                 </div>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <img src={logo} height={160} alt='Zeeshan' />
-                <div><h1>The<span>Zeeshan</span>Group</h1></div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img style={{filter: 'drop-shadow(0 0 4px #000)'}} src={logo} height={160} alt='Zeeshan' />
+                <div><h1>The<br/><span style={{fontSize: '60px', lineHeight: '70px'}}>Zeeshan</span><br/>Group</h1></div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                <div style={{ width: '30%' }}>
+                <div style={{ width: '28%' }}>
                     <img className='card' src={trading} />
                     <h1>Zeeshan Traders</h1>
                 </div>
                 <div style={{ width: '30%' }}>
-                    <img className='card' src={construction} />
+                   <a href='https://zeeshan-six.vercel.app/'> <img className='card' src={construction} /> </a>
                     <h1>Zeeshan Construction</h1>
                 </div>
                 <div style={{ width: '30%' }}>
@@ -41,6 +42,7 @@ const Homepage = () => {
                     <h1>Coming Soon</h1>
                 </div>
             </div>
+        </div>
         </div>
     )
 }
